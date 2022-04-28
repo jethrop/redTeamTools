d2204cdec8d4b97b75bd80b6ee1eb7ef4402a5f6
d3thr0 <32994729+jethrop@users.noreply.github.com>
2022-04-27 21:21:37 -0600

    Added more variables

--- quietHours/quietHours.py
+++ quietHours/quietHours.py
@@ -1,8 +1,31 @@
 #!
-##Variables
-#All variables must be in 24hr YYMMDD format
-##Start Time
-sT = int(2200)
-##End Time
-eT = int(0500)
+##Imports/dependencies
+#!! Include dependancy installing code?
+# Imports for using system commands
+import sys
 
+#Imports for using pure python in functions
+import asyncio
+import pyrcrack
+from rich.console import Console
+from rich.prompt import Prompt
+
+###Variables
+##All variables must be in 24hr YYMMDD format
+
+## Timing variables
+#Start Time
+sT = 2200
+#End Time
+eT = 0500
+#Dos Interval in minutes
+dosMin = 3
+dosMax = 10
+
+## Data variables
+# Target Mac Address
+tMac = None
+# Router Mac Address
+rMac = None
+#Video buffer size kb
+vSize = 100
